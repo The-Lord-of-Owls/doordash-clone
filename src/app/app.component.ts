04b6c216fc3ba8c2d2ba04ec554eab6f3f42a062
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2024-04-23 12:58:09 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { jsPDF } from "jspdf"
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [ RouterOutlet ],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'doordash-clone';
+	title = 'doordash-clone'
+	fileContent = "<h1>Hello World!</h1>"
+	filePath = "helloworld.pdf"
+
+	generatePDF() {
+		const doc = new jsPDF()
+
+		doc.html( this.fileContent, {
+			callback: pdf => pdf.save( this.filePath )
+		} )
+	}
 }
